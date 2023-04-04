fe52f1cd8af78d9ea0f2a91232f1b5318fa8ed1e
MainulDip <mainuldip@gmail.com>
2023-04-03 23:17:23 -0400

    cstudy array as fn parameter init

---
+++ Apps/ArrayIntermediate/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printSizeOf(int intArray[]);
+void printLength(int intArray[]);
+
+int main(int argc, char* argv[])
+{
+    int array[7] = { 0, 1, 2, 3, 4, 5, 6 };
+
+    printf("sizeof of array: %d\n", (int) sizeof(array)); // 28
+
+    printf("sizeof of array's first element: %d\n", (int) sizeof(array[0])); // 4
+    printf("sizeof of array's first element address: %d\n", (int) sizeof(&array[0])); // 8
+    printf("sizeof of array's first element: %d\n", (int) sizeof(&array)); // 8
+
+    printSizeOf(array); // 8
+    // An array sent as a parameter to a function is treated as a pointer address, so sizeof will return the pointer's address size, instead of the array's.
+
+    printf("Length of array: %d\n", (int)( sizeof(array) / sizeof(array[0]) )); // 7
+    printLength(array); // 2
+}
+
+void printSizeOf(int intArray[])
+{
+    printf("sizeof of parameter: %d\n", (int) sizeof(intArray)); // 8, address size
+    printf("intArray = %d\n", intArray); // 6422016 (memory address)
+}
+
+void printLength(int intArray[])
+{
+    // here intArray is a address, so
+    printf("Length of parameter: %d\n", (int)( sizeof(intArray) / sizeof(intArray[0]) ));
+    printf("\n\n%d", (int) sizeof(intArray));
+}
--- README.md
+++ README.md
@@ -180,7 +180,51 @@ void printLength(int intArray[])
     printf("Length of parameter: %d\n", (int)( sizeof(intArray) / sizeof(intArray[0]) ));
 }
 ```
+### Function Parameter as Pointer and Address:
+* Passing Address
+```c
+#include <stdio.h>
+void swap(int *n1, int *n2);
 
+int main()
+{
+    int num1 = 5, num2 = 10;
+
+    // address of num1 and num2 is passed
+    swap( &num1, &num2);
+
+    printf("num1 = %d\n", num1);
+    printf("num2 = %d", num2);
+    return 0;
+}
+
+void swap(int* n1, int* n2)
+{
+    int temp;
+    temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+}
+```
+
+* Passing Pointer
+```c
+#include <stdio.h>
+
+void addOne(int* ptr) {
+  (*ptr)++; // adding 1 to *ptr
+}
+
+int main()
+{
+  int* p, i = 10;
+  p = &i;
+  addOne(p);
+
+  printf("%d", *p); // 11
+  return 0;
+}
+```
 
 ### Return Statement :
 
